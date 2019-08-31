fbe4136105a6b1ad90d9fd15a1dd52afcb28a067
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2019-08-31 12:11:27 +0200

    stable security
    secureMode is now a constant and cannot be changed. more readable code. added more throw messages.

--- string-obfuscator.js
+++ string-obfuscator.js
@@ -1,91 +1,97 @@
 class stringObfuscator {
 
-  constructor( forceSecureMode ) {
+  constructor( secureMode ) {
 
-    this.forceSecureMode = typeof forceSecureMode === "boolean" ? forceSecureMode : true;
+    secureMode = typeof secureMode === "boolean" ? secureMode : true;
     this.__proto__.supportedCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","à","è","é","ì","ò","ù","0","1","2","3","4","5","6","7","8","9",".",",",";",":","!","?","\"","'","\\","{","}","[","]","(",")","&","|","~","*","#","<",">","@","%","_","-","+","=","/","`","$","€","^"," ","  "];
-    this.__proto__.secureMode = this.forceSecureMode
+    this.__proto__.secureMode = secureMode;
+    this.defineSecureMode()
 
   }
 
   encode( string, key ) {
 
-    let noInvalidCharacters = true;
-
     if ( typeof string !== "undefined" && typeof key !== "undefined" ) {
 
-      string = this.removeSpecialCharacters( String( string ) ); key = this.removeSpecialCharacters( String( key ) );
+      string = this.stringFormatting( string );
+      key = this.stringFormatting( key );
 
-      string.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
-      key.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
+      const newCharactersArray = this.recreateCharacterOrder( this.getKeyNumberValue( key ) );
+      const result = string.split("").map( v => newCharactersArray[ this.supportedCharacters.indexOf( v ) ] ).join("");
 
-      if ( noInvalidCharacters === true ) {
-
-        const newCharactersArray = this.recreateCharacterOrder( this.getKeyNumberValue( key ) );
-        const result = string.split("").map( v => newCharactersArray[ this.supportedCharacters.indexOf( v ) ] ).join("");
-
-        return this.secureMode === true ? { result: result, signature: key.split("").map( v => newCharactersArray[ this.supportedCharacters.indexOf( v ) ] ).join("") } : result
-
-      }
+      return window.secureMode === true ? { result: result, signature: key.split("").map( v => newCharactersArray[ this.supportedCharacters.indexOf( v ) ] ).join("") } : result
 
     }
+
+    else {throw TypeError("correct usage: encode( string, key )")}
 
   }
 
   decode( string, key ) {
 
-    let noInvalidCharacters = true;
-
-    if ( this.secureMode === false ) {
+    if ( window.secureMode === false ) {
 
       if ( typeof string !== "undefined" && typeof key !== "undefined" ) {
 
-        string = this.removeSpecialCharacters( String( string ) ); key = this.removeSpecialCharacters( String( key ) );
+        string = this.stringFormatting( string );
+        key = this.stringFormatting( key );
 
-        string.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
-        key.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
+        const newCharactersArray = this.recreateCharacterOrder( this.getKeyNumberValue( key ) );
 
-        if ( noInvalidCharacters === true ) {
-
-          const newCharactersArray = this.recreateCharacterOrder( this.getKeyNumberValue( key ) );
-
-          this.secureMode = this.forceSecureMode;
-
-          return string.split("").map( v => this.supportedCharacters[ newCharactersArray.indexOf( v ) ] ).join("")
-
-        }
+        return string.split("").map( v => this.supportedCharacters[ newCharactersArray.indexOf( v ) ] ).join("")
 
       }
 
+      else {throw TypeError("correct usage: decode( string, key )")}
+
     }
+
+    else {throw TypeError("can't use this function while secure mode is active")}
 
   }
 
   secureDecode( string, key, signature ) {
 
-    let noInvalidCharacters = true;
+    let newCharactersArray;
 
     if ( typeof string !== "undefined" && typeof key !== "undefined" && typeof signature !== "undefined" ) {
 
-      string = this.removeSpecialCharacters( String( string ) ); key = this.removeSpecialCharacters( String( key ) ); signature = this.removeSpecialCharacters( String( signature ) );
+      string = this.stringFormatting( string );
+      key = this.stringFormatting( key ); 
+      signature = this.stringFormatting( signature );
 
-      string.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
-      key.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
-      signature.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
+	  newCharactersArray = this.recreateCharacterOrder( this.getKeyNumberValue( key ) );
+	  const decodedSignature = signature.split("").map( v => this.supportedCharacters[ newCharactersArray.indexOf( v ) ] ).join("")
 
-      if ( noInvalidCharacters === true ) {
+      if ( decodedSignature === key ) {
 
-        this.secureMode = false;
-        const decodedSignature = this.decode( signature, key );
-
-        if ( decodedSignature === key ) {
-
-          this.secureMode = false;
-          return this.decode( string, key );
-
-        }
+	    return string.split("").map( v => this.supportedCharacters[ newCharactersArray.indexOf( v ) ] ).join("")
 
       }
+
+    }
+
+    else {throw TypeError("correct usage: secureDecode( string, key, signature )")}
+
+  }
+
+  stringFormatting( string ) {
+
+    if ( typeof string !== "undefined" ) {
+
+    	string = this.removeSpecialCharacters( String( string ) ); 
+    	this.checkForInvalidCharacters( string );
+    	return string
+
+    }
+
+  }
+
+  checkForInvalidCharacters( string ) {
+
+  	if ( typeof string !== "undefined" ) {
+
+  	  string.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { throw TypeError( `"${v}" is not a supported character` ) } } )
 
     }
 
@@ -107,26 +113,18 @@ class stringObfuscator {
 
       return [ ...supportedCharacters.slice( keyNumberValue, supportedCharacters.length - 1 ), ...supportedCharacters.slice( 0, keyNumberValue ) ]
 
-    } 
+    }
 
   }
 
   getKeyNumberValue( key ) {
 
-    let noInvalidCharacters = true;
-
     if ( typeof key !== "undefined" ) {
 
-      const keyCharacters = this.removeSpecialCharacters( String( key ) ); 
-      const characterOrderValue = keyCharacters.split("").map( v => this.supportedCharacters.indexOf( v ) ).reduce( ( a, b ) => Math.abs( a * 2 - b ) );
+      key = this.stringFormatting( key ); 
+      const characterOrderValue = key.split("").map( v => this.supportedCharacters.indexOf( v ) ).reduce( ( a, b ) => Math.abs( a * 2 - b ) );
 
-      keyCharacters.split("").forEach( v => { if ( this.supportedCharacters.indexOf( v ) === -1 ) { noInvalidCharacters = false; throw TypeError( `"${v}" is not a supported character` ) } } )
-
-      if ( noInvalidCharacters === true ) {
-
-        return Math.floor( ( keyCharacters.split("").map( v => this.supportedCharacters.indexOf( v ) + 1 ).reduce( ( a, b ) => a + b ) + characterOrderValue * 3 ) * 8 ) 
-
-      }
+      return Math.floor( ( key.split("").map( v => this.supportedCharacters.indexOf( v ) + 1 ).reduce( ( a, b ) => a + b ) + characterOrderValue * 3 ) * 8 ) 
 
     }
 
@@ -139,6 +137,24 @@ class stringObfuscator {
       string = String( string );
 
       return string.replace( /(\r\n\t|\n|\r\t)/gm ,"")
+
+    }
+
+  }
+
+  defineSecureMode(){
+
+  	const name = "secureMode",
+  	value = this.__proto__.secureMode;
+
+  	if ( typeof window.secureMode === "undefined" ) {
+
+      Object.defineProperty(window, name, { 
+
+         get: function(){return value},
+         set: function(){throw(name+' is a constant and cannot be redeclared.')}
+
+      })
 
     }
 
